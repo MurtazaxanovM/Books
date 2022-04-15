7517bc73b0def727265a243620da1fca1c03a34c
MurtazaxanovM <murtazaxanovm@gmail.com>
2022-04-15 13:04:22 +0500

    Добавьте файлы проекта.

---
+++ BookOnline/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookOnline.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+    }
+}
